ef7a5459ab05657b745ce7e462b9a5b6e7a004de
bble_codes <65161048+dvjnpojascodes@users.noreply.github.com>
2021-08-29 21:47:47 +0800

    Add files via upload

---
+++ index/questions.js
@@ -0,0 +1,122 @@
+//creating an array and passing the number, questions and options
+
+let questions = [
+  {
+  numb: 1,
+  question: "A small, membrane-bound sac that transports or stores materials within cells",
+  answer: "Vesicle",
+   options: [
+     "Endoplasmic Reticulum",
+     "Peroxisomes",
+     "Vesicle",
+     "Lysosome",
+   ]
+},
+{
+  numb: 2,
+  question: "Attached by cerebellar peduncles to the brain-stem",
+  answer: "Cerebellum",
+  options: [
+  "Cerebellum",
+  "Cerebrum",
+   "Thalamus",
+   "Basal Nuclei"
+ ]
+},
+
+{
+  numb: 3,
+  question: "The major sites of adenosine triphosphate",
+  answer: "Mitochondria",
+  options: [
+  "Mitochondria",
+  "Endoplasmic Reticulum",
+  "Centrioles",
+  "Ribosomes"
+ ]
+},
+
+{
+  numb: 4,
+  question: "Two small glands that are located superior to each kidney",
+  answer: "Adrenal Medulla",
+  options: [
+  "Adrenal Medulla",
+  "Adrenal Cortex",
+  "Adrenal Glands",
+  "Anterior Pituitary"
+ ]
+},
+
+{
+  numb: 5,
+  question: " Which of the following best describes the human body's defense mechanism against environmental bacteria?",
+  answer: "Mucous membranes",
+  options: [
+  "Mucous membranes",
+  " Hair in the nose",
+  "Saliva",
+  "Tears"
+ ]
+},
+
+{
+  numb: 6,
+  question: " Which cells in the blood do not have a nucleus? ",
+  answer: "Erythrocyte",
+  options: [
+  "Lymphocyte",
+  "Neutrophil",
+  "Erythrocyte",
+  "Monocyte"
+ ]
+},
+
+{
+  numb: 7,
+  question: "Which of the following allows air to pass into the lungs?",
+  answer: "Trachea",
+  options: [
+  "Heart",
+  "Aorta",
+  "Larynx",
+  "Trachea"
+ ]
+},
+
+{
+  numb: 8,
+  question: "Which of the following closes and seals off the lower airway during swallowing?",
+  answer: "Epiglottis",
+  options: [
+  "Epiglottis",
+  "Larynx",
+  "Uvula",
+  "Vocal cords"
+ ]
+},
+
+{
+  numb: 9,
+  question: "Which of the following is a structural, fibrous protein found in the dermis?",
+  answer: "Collagen",
+  options: [
+  "Heparin",
+  "Melanin",
+  "Collagen",
+  "Sebum"
+ ]
+},
+
+{
+  numb: 10,
+  question: "Which of the following is the point at which an impulse is transmitted from one neuron to another neuron?",
+  answer: "Synapse",
+  options: [
+  "Dendrite",
+  "Terminal plate",
+  "Glial cell",
+  "Synapse"
+ ]
+},
+];
